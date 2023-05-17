5c3ffc3115ba532d3d4bd84c5edad484dd2f008c
AhnMinWoo <minw95@kyobobook.com>
2023-05-17 20:25:46 +0900

    feat(release): cross-env 설정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kyobo-fe'
+  // github 저장소 이름
+  base: isProduction ? '/kyobo-fe' : '/'
 })
