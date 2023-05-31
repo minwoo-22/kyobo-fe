b4fe6fd7c8bab934712d77324b54ebe4c1001e9b
AhnMinWoo <minw95@kyobobook.com>
2023-05-31 19:47:35 +0900

    feat: tailwindcss 추가

---
+++ src/components/board.jsx
@@ -0,0 +1,53 @@
+import { Square } from './square'
+import { calculateWinner } from '../func/calc'
+
+export function Board ({ xIsNext, squares, onPlay }) {
+  const winner = calculateWinner(squares)
+
+  function handleClick (i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    const nextSquares = squares.slice()
+    if (xIsNext) {
+      nextSquares[i] = 'X'
+    } else {
+      nextSquares[i] = 'O'
+    }
+    onPlay(nextSquares, winner)
+  }
+
+  // const winner = calculateWinner(squares)
+  // const [isDone, setIsDone] = useState(winner !== null)
+
+  let status
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+  }
+
+  return (
+    // <></> 문법은 여러 개의 element를 반환하기 위한 문법이다.
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  )
+}
+
+export default Board
---
+++ src/components/square.jsx
@@ -0,0 +1,10 @@
+export function Square (props) {
+  const { value, onSquareClick } = props
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  )
+}
+
+export default Square
